79e7f5f40136e5308aaebe362e2c67da199ecc35
carolina123123 <carolrsc1411@gmail.com>
2025-03-13 09:26:11 -0300

    Aula 01

---
+++ Aula01.py
@@ -0,0 +1,18 @@
+a = 10.2;
+b = "Oi";
+
+# Float e String
+print(type(a));
+print(type(b));
+
+# Transforma ambos pra booleano (True/False)
+a = bool(a);
+b = bool(b);
+
+# Para tudo que é diferente de zero, é sempre True e 1
+print("Bool pra int: ")
+print(int(a));
+print(int(b));
+
+# Concatenação: 
+print(f"A varíavel B é: {b} e a A é: {int(a)}");
